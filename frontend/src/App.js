c4de4976dbe85c3f8e494460080c7ec77bc273b1
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-03-22 23:35:18 +0400

    practice done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,6 +27,7 @@ import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
 
 const router = createBrowserRouter([
   {
@@ -36,11 +37,17 @@ const router = createBrowserRouter([
       { index: true, element: <HomePage /> },
       {
         path: "events",
-        element: <EventsPage />,
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+          },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
       },
-      { path: "events/:eventId", element: <EventDetailPage /> },
-      { path: "events/new", element: <NewEventPage /> },
-      { path: "events/:eventId/edit", element: <EditEventPage /> },
     ],
   },
 ]);
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -3,5 +3,9 @@ import { useParams } from "react-router-dom";
 export default function EventDetailPage() {
   const { eventId } = useParams();
 
-  return <div>{eventId}</div>;
+  return (
+    <div>
+      <p>Event ID: {eventId}</p>
+    </div>
+  );
 }
