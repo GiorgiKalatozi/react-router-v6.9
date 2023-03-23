735cb58cac37bf025375a943b6b396ae9af22335
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-03-23 21:23:37 +0400

    loader code

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,11 +1,7 @@
 import MainNavigation from "../components/MainNavigation";
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
-  const data = useLoaderData();
-
-  console.log(data);
-
   return (
     <>
       <MainNavigation />
