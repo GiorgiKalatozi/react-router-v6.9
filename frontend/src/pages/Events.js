8760c3b79199382012a5233b0bdbbf53404d380d
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-01 01:35:27 +0400

    error handeling

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -5,6 +5,10 @@ function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
 
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
+
   return <EventsList events={events} />;
 }
 
@@ -14,7 +18,8 @@ export async function loader() {
   const response = await fetch("http://localhost:5000/events");
 
   if (!response.ok) {
-    //
+    // return { isError: true, message: "Could not fetch events." };
+    throw new Error();
   } else {
     return response;
   }
