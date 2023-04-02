46db4e3fbd6f9941da31ae65eb8569a4f244c024
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-02 20:00:50 +0400

    error data and throwing responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -19,7 +19,9 @@ export async function loader() {
 
   if (!response.ok) {
     // return { isError: true, message: "Could not fetch events." };
-    throw new Error();
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
   } else {
     return response;
   }
