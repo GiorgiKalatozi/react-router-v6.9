d78903627e17dee9c8ec7032a45230dd0f17a5ad
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-03 20:30:10 +0400

    cors fk u

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,11 +1,26 @@
-import { useParams } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
-  const { eventId } = useParams();
+  const data = useLoaderData();
 
+  console.log(data, "sdsd");
   return (
     <div>
-      <p>Event ID: {eventId}</p>
+      <EventItem event={data.event} />
     </div>
   );
+}
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+  const response = await fetch("http://localhost:8080/events/" + id);
+  if (!response.ok) {
+    throw json(
+      { message: "Could not fetch details for selected events." },
+      { status: 500 }
+    );
+  } else {
+    return response;
+  }
 }
