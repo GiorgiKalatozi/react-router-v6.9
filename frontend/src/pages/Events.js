cae8ac4601cc857b0424a911def310dbceac3b92
fallenprodigy <giorgi.kalatozishvili.1@btu.edu.ge>
2023-04-05 03:03:09 +0400

    feat: data fetching with defer

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,20 +1,22 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
   const data = useLoaderData();
-  const events = data.events;
 
-  // if (data.isError) {
-  //   return <p>{data.message}</p>;
-  // }
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={data.events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:5000/events");
 
   if (!response.ok) {
@@ -24,6 +26,13 @@ export async function loader() {
     // });
     return json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
